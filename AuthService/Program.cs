87104c84e9e5914e77b2b300a7674ee38e426bf9
LuisRod1990 <luisrodj1990@gmail.com>
2026-01-31 09:12:48 -0600

    CI/CD first deploy

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -13,7 +13,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = "Server=34.60.141.106,1433;Database=DW_Portfolio;User Id=sqlserver;Password=MxN1990A;Encrypt=True;TrustServerCertificate=True;";
+var connectionString = "Server=127.0.0.1,1433;Database=DW_Portfolio;User Id=sqlserver;Password=MxN1990A;Encrypt=True;TrustServerCertificate=True;";
 Console.WriteLine($"Connection String: {connectionString}");
 var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY") ?? "DefaultKey";
 var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? "DefaultIssuer";
