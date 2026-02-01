1951c9cc19715cd3e88df555ea939882ab80c5e2
LuisRod1990 <luisrodj1990@gmail.com>
2026-02-01 13:29:52 -0600

    Prueba despliegue postgresql 7

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -29,11 +29,11 @@ Console.WriteLine($"corsName: {corsName}");
 Console.WriteLine($"corsHost: {corsHost}");
 
 
-// Configuración detallada del DbContext para logging
-builder.Services.AddDbContext<AuthDbContext>(options =>
-    options.UseSqlServer(connectionString)
-           .EnableSensitiveDataLogging()   // muestra la cadena de conexión usada. Eliminar en producción
-           .LogTo(Console.WriteLine, LogLevel.Information)); // log detallado. Eliminar en producción
+//// Configuración detallada del DbContext para logging
+//builder.Services.AddDbContext<AuthDbContext>(options =>
+//    options.UseSqlServer(connectionString)
+//           .EnableSensitiveDataLogging()   // muestra la cadena de conexión usada. Eliminar en producción
+//           .LogTo(Console.WriteLine, LogLevel.Information)); // log detallado. Eliminar en producción
 
 builder.Services.AddCors(options =>
 {
@@ -96,7 +96,10 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IUsuarioSeguridadRepository, UsuarioSeguridadRepository>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
@@ -114,45 +117,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
-
-
-
-//using (var scope = app.Services.CreateScope())
-//{
-//    var services = scope.ServiceProvider;
-//    try
-//    {
-//        var context = services.GetRequiredService<AuthDbContext>();
-//        context.Database.OpenConnection();
-//        Console.WriteLine("✅ Conexión exitosa a Cloud SQL Server");
-//        context.Database.CloseConnection();
-//    }
-//    catch (SqlException ex)
-//    {
-//        Console.WriteLine("❌ SQL ERROR DETALLADO:");
-//        Console.WriteLine($"Message: {ex.Message}");
-//        Console.WriteLine($"Error Number: {ex.Number}");
-//        Console.WriteLine($"Error State: {ex.State}");
-//        Console.WriteLine($"Error Class: {ex.Class}");
-//        Console.WriteLine($"Server: {ex.Server}");
-//        Console.WriteLine($"Source: {ex.Source}");
-//        Console.WriteLine($"Procedure: {ex.Procedure}");
-//        Console.WriteLine($"LineNumber: {ex.LineNumber}");
-//        Console.WriteLine($"StackTrace: {ex.StackTrace}");
-//        Console.WriteLine($"ClientConnectionId: {ex.ClientConnectionId}");
-//    }
-//    catch (Exception ex)
-//    {
-//        Console.WriteLine("❌ ERROR GENERAL DETALLADO:");
-//        Console.WriteLine($"Message: {ex.Message}");
-//        Console.WriteLine($"StackTrace: {ex.StackTrace}");
-//        if (ex.InnerException != null)
-//        {
-//            Console.WriteLine($"InnerException: {ex.InnerException.Message}");
-//        }
-//    }
-//}
 
 
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
